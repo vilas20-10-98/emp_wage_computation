4d853b18d367b083f8da3f55ddb878f3fd153465
vilas20-10-98 <2016BPR030@sggs.ac.in>
2020-08-05 20:27:58 +0530

    emp wage added

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -70,9 +70,23 @@ do
   totalsalary=$(($emphrs*$emprateperhrs));
 done
 
+isPARTTIME=1;
+isFULLTIME=2;
+emprateperhrs=20;
+randomcheck=$((RANDOM%3));
 
+if [ $isFULLTIME -eq $randomcheck ];
+then
+  emphrs=8;
 
+elif [ $isPARTTIME -eq $randomcheck ];
+then
+   emphrs=4;
+else
+   emphrs=0;
+fi
 
+salary=$(($emphrs*$emprateperhrs));
 
 
 
