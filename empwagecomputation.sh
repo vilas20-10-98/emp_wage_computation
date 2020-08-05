6a2244b8679c7fb8cd9272f6f75283fbc04e5f43
vilas20-10-98 <2016BPR030@sggs.ac.in>
2020-08-05 19:37:39 +0530

    emp attendance

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -2,5 +2,20 @@
 
 echo "welcome to emp wage computation";
 
+isPRASENT=1;
+isABSCENT=2;
+emp_check=$(( RANDOM%2 ));
+
+
+if [ $isPRASENT -eq $emp_check ]
+
+then
+  echo "employee is prasent";
+else
+
+  echo "employee is abscent";
+fi
+
+
 
 
