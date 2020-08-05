ec12134e81d9450b3c057547abb0c32a932cb30b
vilas20-10-98 <2016BPR030@sggs.ac.in>
2020-08-05 19:50:54 +0530

    wage calculation

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -16,6 +16,34 @@ else
   echo "employee is abscent";
 fi
 
+isPARTTIME=1;
+isFULLTIME=2;
+emprateperhrs=20;
+salary=0;
+numworkingdays=20;
+
+
+for (( day=1; day<=$numworkingdays; day++ ))
+
+do
+
+   empcheck=$(( RANDOM%3 ));
+  case $empcheck in
+    $isFULLTIME)
+         emphrs=8
+          ;;
+    $isPARTTIME)
+         emphrs=4
+         ;;
+     *)
+        emphrs=0
+        ;;
+ esac
+  salary=$(($emhrs*$emprateperhrs));
+  totalsalary=$(($emphrs*$emprateperhrs));
+done
+
+
 
 
 
