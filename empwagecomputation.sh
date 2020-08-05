a11123e78ae0158b7710be47e63e151ff5275c2c
vilas20-10-98 <2016BPR030@sggs.ac.in>
2020-08-05 20:00:08 +0530

    emp part and full adding

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -43,6 +43,35 @@ do
   totalsalary=$(($emphrs*$emprateperhrs));
 done
 
+isPARTTIME=1;
+isFULLTIME=2;
+emprateperhrs=20;
+salary=0;
+numworkingdays=20;
+
+
+for (( day=1; day<=$numworkingdays; day++ ))
+
+do
+
+   empcheck=$(( RANDOM%3 ));
+  case $empcheck in
+    $isFULLTIME)
+         emphrs=8
+          ;;
+    $isPARTTIME)
+         emphrs=4
+         ;;
+     *)
+        emphrs=0
+        ;;
+ esac
+  salary=$(($emhrs*$emprateperhrs));
+  totalsalary=$(($emphrs*$emprateperhrs));
+done
+
+
+
 
 
 
