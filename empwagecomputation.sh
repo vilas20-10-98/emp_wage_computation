c594b58d14f65f8ebc8119e7b7932111a444d083
vilas20-10-98 <2016BPR030@sggs.ac.in>
2020-08-06 15:56:27 +0530

    store daily wage along with total wage

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -155,3 +155,41 @@ done
 
 totalsalary=$(($totalworkingdays*$EMP_RATE_PER_HRS));
 
+
+
+#!/bin/bash -x
+
+is_PART_TIME=1;
+is_FULL_TIME=2;
+max_hrs_in_mon=100;
+emp_rate_per_hrs=20;
+num_working_days=20;
+
+totalEMPHRS=0;
+totalworkingdays=0;
+
+
+while [[ $totalEMPHRS -lt $max_hrs_in_mon && $totalworkingdays -lt $num_working_days ]]
+do
+  ((totalworkingdays++))
+  empcheck=$((RANDOM%3));
+  case $empcheck in
+    $is_FULL_TIME)
+       emphrs=8;
+       ;;
+   $is_PART_TIME)
+      emphrs=4;
+      ;;
+   *)
+    emphrs=0;
+     ;;
+esac
+totalEMPHRS=$(($totalEMPHRS+$emphrs))
+
+totalsalary=$(($totalEMPHRS*$emp_rate_per_hrs));
+
+x=$(($totalEMPHRS+$totalsalary));
+
+done
+
+
